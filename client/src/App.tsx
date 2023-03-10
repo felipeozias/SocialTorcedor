9db196a703d60d76da934f05f3afe2441f0c2de6
Alan <alan.douglas.ads@gmail.com>
2023-03-10 18:01:38 -0300

    merge conflit resolved

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,5 @@
 import Routes from "./routes";
 
 export default function App() {
-<<<<<<< HEAD
     return <Routes />;
-=======
-    return (
-        /*  <Home /> */
-        <Login />
-    );
->>>>>>> 2b00b2e4f6243c09c68eb98a417693a168f571ec
 }
