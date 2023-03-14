dd18af8e5df319d4ec62593dd3e7ee80c300f6e0
Adriele Maria <adrieleifrn@gmail.com>
2023-03-14 19:21:00 -0300

    alterando a rota do app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,12 @@ import Routes from "./routes";
 // import Home from "./pages/Home";
 // import Login from "./pages/Login";
 // import Register from "./pages/Register";
-import Update from "./pages/Update";
+//import Update from "./pages/Update";
 
 export default function App() {
     return (
         /*<Home />*/ 
         
-        <Update />
+        <Routes />
     );
 }
