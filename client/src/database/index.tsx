2a0a5e1e82af08cff67e7f1d49da55d9aa03b6f3
LucasCscBks <luke100191@gmail.com>
2023-03-17 13:38:09 -0300

    update group, create insert group and exit group, some interfaces, update modal and database....

--- client/src/database/index.tsx
+++ client/src/database/index.tsx
@@ -1,91 +1,23 @@
-export interface IUser {
-    _id: string,
-    name: string
-}
-interface IGroupUser {
-    user: string
-}
 
-export interface IGroup {
-    _id: string,
-    name: string,
-    owner: string,
-    image: string,
-    users: IGroupUser[]
-}
-
-export const simulateLogin = 
-    {
-        _id: "hee1nn2nudnuniunnd2",
-        name: "Rodrigo tebas"
+export const simulateLogin = {
+        _id: "640f6af6ad964b6d45a13c35",
+        name: "Zoro Oliveira",
+        nickname: "zoro_oliveira",
+        team: "são paulo"
     }
 
-export const simulateDbGroups: IGroup[] = [
-    {
-        _id: "eueu2e2uuddddd",
-        name: "Grupo foda",
-        owner: "Rodrigo tebas",
-        image: "bola",
-        users: [
-            {user: "Xandy aviao"},
-            {user: "Rodolfo"},
-            {user: "Milena"}
-        ]
-    },
-    {
-        _id: "eueu494094dddd",
-        name: "Flamenguistas Mengo",
-        owner: "Fla bnobmed",
-        image: "escudo",
-        users: [
-            {user: "Rodrigo tebas"},
-            {user: "Chaves"},
-            {user: "Adilson nunes"}
-        ]
-    },
-    {
-        _id: "eueu2e2uudff3f333333d",
-        name: "Os fodas",
-        owner: "tebas",
-        image: "campo",
-        users: [
-            {user: "Xandy aviao"},
-            {user: "Rodol aha"},
-        ]
-    },
-    {
-        _id: "eueu2e2uudddddji3jj34j3uh43",
-        name: "Grupo fodaastico",
-        owner: "Marina Rodrigo alves",
-        image: "passaro de fogo",
-        users: [
-            {user: "Xandy aviao"},
-            {user: "Rodolfo"},
-            {user: "Rodrigo tebas"},
-            {user: "Milena"},
-            {user: "Chhhha"},
-            {user: "Titio"},
-        ]
-    },
-    {
-        _id: "eueu2e2uudddddji3jj34j3uh43",
-        name: "Grupo de teste",
-        owner: "Rodrigo tebas",
-        image: "passarinho",
-        users: [
-            {user: "Xandy aviao"},
-            {user: "Rodolfoss"},
-            {user: "Cha"},
-            {user: "Titios"},
-        ]
-    }
-]
+export const simulateLogin2 = {
+    _id: "640ff195438d50338971dec8",
+    name: "Shinobu Sensui",
+    nickname: "sensui123",
+    team: "Outro"
+}
 
 export async function apiRequestUsers() {
 
     try {
         const url = process.env.REACT_APP_USERS_LOCAL as string
-        console.log(url)
+        
         const request = await fetch(url);
         if ( !request.ok ) {
         console.error(request.json())
@@ -97,6 +29,37 @@ export async function apiRequestUsers() {
     }
         const res = await request.json();
 
+        return {
+            status: request.status,
+            succesfull: true,
+            data: res["data"]
+        }
+
+    } catch (error) {
+        console.error(error)
+        return {
+            succesfull: false,
+            error: error
+        }
+    }
+}
+
+export async function apiRequestGroups() {
+    try {
+        
+        const url = process.env.REACT_APP_GROUP_LOCAL as string
+
+        const request = await fetch(url);
+        if ( !request.ok ) {
+        console.error(request.json())
+        return {
+            status: request.status,
+            succesfull: false,
+            error: await request.json()
+        }
+    }
+        const res = await request.json();
+        
         return {
             status: request.status,
             succesfull: true,
---
+++ client/src/services/createGroup.tsx
@@ -0,0 +1,29 @@
+
+export default async function createGroup(userData: any) {
+    console.log(userData);
+    try {
+        const options = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(userData),
+        };
+
+        const url: string = process.env.REACT_APP_GROUP_LOCAL as string;
+        const res = await fetch(url, options);
+
+        if (!res.ok) {
+            console.error("Erro ao fazer requisição", await res.json());
+            return { status: res.status };
+        }
+
+        const data = await res.json();
+        console.log(data);
+
+        return { status: res.status, data: data };
+    } catch (err) {
+        alert(
+            "Houve um erro ao criar o grupo. Por favor tente novamente!"
+        );
+        console.error(err);
+    }
+}
