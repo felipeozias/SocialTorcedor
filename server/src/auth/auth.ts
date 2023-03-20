9f68a5a91fa7c92feb4f3446b86edf584c82260f
Diego Oliveira <diegoc.oliveira@gmail.com>
2023-03-20 02:02:01 -0300

    refac: websocket feed com pub/sub

--- server/src/auth/auth.ts
+++ server/src/auth/auth.ts
@@ -2,39 +2,25 @@ import { NextFunction, Request, Response } from "express";
 import UserService from "../services/user-service";
 import IResult from "../interfaces/iresult";
 import IUser from "../interfaces/iuser";
-import jwt from "jsonwebtoken";
 import Logger from "../logger/logger";
-import RedisDb from "../database/redisdb";
+import { v4 as uuidv4 } from "uuid";
+import { redis } from "../database/redisdb";
 
 export default class Auth {
-    constructor() {}
-
     static async authLogin(req: Request, res: Response, next: NextFunction) {
         try {
             const nickname = req.body.nickname.toLowerCase() || "";
             const password = req.body.password || "";
             const service = new UserService();
-            const result: IResult<IUser> = await service.authenticate(
-                nickname,
-                password
-            );
+            const result: IResult<IUser> = await service.authenticate(nickname, password);
 
             if (result.status === 404) {
                 return res.status(401).json(result.errors);
             }
 
-            const token: string = jwt.sign(
-                {
-                    nickname: result.data?._id,
-                },
-                result.data?.nickname as string,
-                { expiresIn: 3600 }
-            );
+            const token: string = uuidv4();
 
-            const client = RedisDb.client();
-            client.set(token, JSON.stringify(result.data), {
-                EX: 3600,
-            });
+            redis.set(token, result.data);
 
             req.body.token = token;
             next();
@@ -54,15 +40,11 @@ export default class Auth {
         }
 
         try {
-            const client = RedisDb.client();
-            const user = await client.get(token);
+            const user = await redis.get(token);
 
-            if (!user)
-                return res
-                    .status(401)
-                    .json({ error: "Token de sessão inválido" });
+            if (!user) return res.status(401).json({ error: "Token de sessão inválido" });
 
-            req.body.userData = JSON.parse(user);
+            req.body.userData = user;
             next();
         } catch (err) {
             Logger.error("Houve um erro ao recuperar token de sessão", err);
@@ -75,12 +57,10 @@ export default class Auth {
     static async logout(req: Request, res: Response) {
         const token: string = req.headers.authorization as string;
 
-        if (!token)
-            return res.status(401).json({ error: "Token de sessão ausente" });
+        if (!token) return res.status(401).json({ error: "Token de sessão ausente" });
 
         try {
-            const client = RedisDb.client();
-            await client.del(token || "");
+            await redis.del(token || "");
             res.status(200).json({ message: "Sucesso ao fazer logout!" });
         } catch (err) {
             Logger.error("Houve um erro ao excluir token de sessão", err);
@@ -92,18 +72,9 @@ export default class Auth {
 
     static async createSession(data: any) {
         try {
-            const token: string = jwt.sign(
-                {
-                    nickname: data._id,
-                },
-                data.nickname as string,
-                { expiresIn: 3600 }
-            );
+            const token: string = uuidv4();
 
-            const client = RedisDb.client();
-            client.set(token, JSON.stringify(data), {
-                EX: 3600,
-            });
+            redis.set(token, data);
             return {
                 message: "Sessão criada com sucesso",
                 error: false,
