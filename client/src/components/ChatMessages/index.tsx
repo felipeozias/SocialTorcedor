53aac682af59350a9a21c73eef05a375ae6cc11f
Alan <alan.douglas.ads@gmail.com>
2023-03-23 13:06:19 -0300

    feat: add action on click of chat

--- client/src/components/ChatMessages/index.tsx
+++ client/src/components/ChatMessages/index.tsx
@@ -1,57 +1,20 @@
-import { StyledChatBoxMessages, StyleMessage } from "./styles";
+import { useEffect, useRef } from "react";
+import { StyledChatBoxMessages } from "./styles";
 
 interface IPropsChat {
     children: any;
 }
 
 export default function ChatMessages(props: IPropsChat) {
+    const divMessages = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        if (divMessages.current) {
+            divMessages.current.scrollTop = divMessages.current.scrollHeight;
+        }
+    }, [props.children]);
     return (
-        <StyledChatBoxMessages>
+        <StyledChatBoxMessages ref={divMessages}>
             {props.children}
-            {/* <StyleMessage>
-                <span>Torcedor certo:</span>
-                <p>Fala mano, beleza?</p>
-            </StyleMessage>
-
-            <StyleMessage>
-                <span>Novo jogador:</span>
-                <p>Fala mano, eai ✌</p>
-            </StyleMessage>
-
-            <StyleMessage>
-                <span>Torcedor certo:</span>
-                <p>Vai ter sofrência hoje?</p>
-            </StyleMessage>
-
-            <StyleMessage>
-                <span>Novo jogador:</span>
-                <p>Vamos ganhar bonido 🏅🏆</p>
-            </StyleMessage>
-
-            <StyleMessage>
-                <span>Torcedor certo:</span>
-                <p>Acho que vai da bom também 🎉🎉🎉</p>
-            </StyleMessage>
-
-            <StyleMessage>
-                <span>Novo jogador:</span>
-                <p>Mais tarde rola uns memes em...</p>
-            </StyleMessage>
-
-            <StyleMessage>
-                <span>Torcedor certo:</span>
-                <p>Hahaa, com certeza🎞🎞</p>
-            </StyleMessage>
-
-            <StyleMessage>
-                <span>Novo jogador:</span>
-                <p>Me organizar aqui </p>
-            </StyleMessage>
-
-            <StyleMessage>
-                <span>Torcedor certo:</span>
-                <p>Cadê a galera desse grupo em???💤</p>
-            </StyleMessage> */}
         </StyledChatBoxMessages>
     );
 }
